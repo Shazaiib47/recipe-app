851958fa1a3fb321b10483c4cd5ca78c36e7d7fc
Shazaiib <shazaib47@hotmail.com>
2024-06-03 13:54:46 +0100

    amended font size for home nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
     <header class="bg-white shadow h-16 flex justify-between items-stretch">
       <router-link
         :to="{ name: 'home' }"
-        class="inline-flex items-center h-full px-5 text-orange-500 font-bold"
+        class="inline-flex items-center h-full text-lg px-5 text-orange-500 font-bold"
       >
         Home
       </router-link>
